1602b7cf132fe6d307efc7b7580eef7c4baa2ae8
Kwonhyukjoon <victorehko12@naver.com>
2020-08-12 12:40:47 +0900

    final commit

--- controllers/todo.js
+++ controllers/todo.js
@@ -9,6 +9,11 @@ exports.getTodo = async (req, res, next) => {
   let offset = req.query.offset;
   let limit = req.query.limit;
 
+  if (!offset || !limit) {
+    res.status(400).json({ message: "parameters setting error" });
+    return;
+  }
+
   let query = `select * from todo limit ${offset}, ${limit};`;
   console.log(query);
   try {
@@ -16,5 +21,28 @@ exports.getTodo = async (req, res, next) => {
     res.status(200).json({ success: true, items: rows, cnt: rows.length });
   } catch (e) {
     res.status(500).json({ success: false, message: "DB Error", error: e });
+  }
+};
+
+// @desc      완료 여부 체크 및 해제하는 API
+// @route     put/api/v1/todo/update
+// @request   id , completed
+// @response  completed(boolean)
+exports.updateTodo = async (req, res, next) => {
+  let id = req.body.id;
+  let completed = req.body.completed;
+
+  if (!id || !completed) {
+    res.status(400).json({ message: "parameters setting error" });
+    return;
+  }
+  console.log(completed);
+
+  let query = `update todo set completed = ${completed} where id = ${id}`;
+  try {
+    [result] = await connection.query(query);
+    res.status(200).json({ success: true, result: result });
+  } catch (e) {
+    res.status(400).json({ success: false, error: e });
   }
 };
--- routes/todo.js
+++ routes/todo.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getTodo } = require("../controllers/todo");
+const { getTodo, updateTodo } = require("../controllers/todo");
 
 const router = express.Router();
 router.route("/").get(getTodo);
+router.route("/update").put(updateTodo);
 
 module.exports = router;
