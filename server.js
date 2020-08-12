c98b5b1f3479eb591418ae24d74a817a9a6a21ce
Kwonhyukjoon <victorehko12@naver.com>
2020-08-12 10:01:11 +0900

    commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res, next) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log("App listening on port 4000!");
+});
